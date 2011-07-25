850dda5a29b0de4991f2f596e4e6536fbfc7b919
Sam Oakley <sam@blork.co.uk>
2011-07-25 18:41:52 +0100

    Removed whitespace

--- src/com/blork/sowidget/model/QuestionFactory.java
+++ src/com/blork/sowidget/model/QuestionFactory.java
@@ -3,16 +3,17 @@ package com.blork.sowidget.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
+import com.blork.sowidget.provider.FavouritesContentProvider;
 import com.blork.sowidget.provider.QuestionsContentProvider;
 
 public class QuestionFactory {
 	public static List<Question> getSaved(Context context) {
-
-
 		String[] projection = new String[] {
 				QuestionsContentProvider.QUESTION_ID,
 				QuestionsContentProvider.TITLE,
@@ -62,10 +63,82 @@ public class QuestionFactory {
 
 			} while (cursor.moveToNext());
 
+		} else {
+			Log.e("sowidget", "No results!!!");
 		}
 		
 		cursor.close();
 
 		return questionList;
 	}
+	
+	public static List<Question> getFavourites(Context context) {
+		String[] projection = new String[] {
+				QuestionsContentProvider.QUESTION_ID,
+				QuestionsContentProvider.TITLE,
+				QuestionsContentProvider.TAGS,
+				QuestionsContentProvider.SITE,
+				QuestionsContentProvider.USER_NAME,
+				QuestionsContentProvider.VOTES,
+				QuestionsContentProvider.ANSWER_COUNT
+		};
+
+		Uri questions = FavouritesContentProvider.CONTENT_URI;
+
+		//Make the query. 
+		Cursor cursor = context.getContentResolver().query(questions,
+				projection, // Which columns to return 
+				null,       // Which rows to return (all rows)
+				null,       // Selection arguments (none)
+				// Put the results in ascending order by name
+				FavouritesContentProvider.DEFAULT_SORT_ORDER);
+
+		ArrayList<Question> favouritesList = new ArrayList<Question>();
+
+		if (cursor.moveToFirst()) {
+
+			int idColumn = cursor.getColumnIndex(FavouritesContentProvider.QUESTION_ID); 
+			int titleColumn = cursor.getColumnIndex(QuestionsContentProvider.TITLE); 
+			int tagsColumn = cursor.getColumnIndex(FavouritesContentProvider.TAGS); 
+			int userNameColumn = cursor.getColumnIndex(FavouritesContentProvider.USER_NAME); 
+			int siteColumn = cursor.getColumnIndex(FavouritesContentProvider.SITE); 
+			int votesColumn = cursor.getColumnIndex(FavouritesContentProvider.VOTES); 
+			int answerCountColumn = cursor.getColumnIndex(FavouritesContentProvider.ANSWER_COUNT); 
+
+
+			do {
+
+				Question favourite = new Question(
+						cursor.getInt(idColumn),
+						cursor.getString(titleColumn),
+						cursor.getString(tagsColumn),
+						cursor.getString(userNameColumn),
+						cursor.getString(siteColumn),
+						cursor.getInt(votesColumn),
+						cursor.getInt(answerCountColumn)
+				);
+				
+				favouritesList.add(favourite);
+
+			} while (cursor.moveToNext());
+
+		} else {
+			Log.e("sowidget", "No results!!!");
+		}
+		
+		cursor.close();
+
+		return favouritesList;
+	}
+	
+	/**
+	 * Delete all.
+	 *
+	 * @param ctx the ctx
+	 */
+	public static void deleteAll(Context ctx) {
+		ContentResolver resolver = ctx.getContentResolver();
+		resolver.delete(QuestionsContentProvider.CONTENT_URI, null, null);
+	}
+
 }
